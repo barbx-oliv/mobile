edf0cf548b7e4bf8d5f5d416fe9649dc9cd1cf61
Bárbara de Oliveira Cruz <barbara.o.cruz@aluno.senai.br>
2026-04-28 08:01:49 -0300

    28/04

--- widgetEstrutura/intro_interacao/lib/widgets/bnbwidget.dart
+++ widgetEstrutura/intro_interacao/lib/widgets/bnbwidget.dart
@@ -1,18 +1,15 @@
-
-
 import 'package:flutter/material.dart';
 // import 'package:intro_interacao/pages/contato_page.dart';
 // import 'package:intro_interacao/pages/formulario_page.dart';
 // import 'package:intro_interacao/pages/home_page.dart';
 
-int pagesIndex = 0;
-
-final List<Widgets> _pages = [
-  HomePage(), FormularioPage(), ContatoPage()
-];
-
-class Bnbwidget {
-  BottomNavigationBar(
-    
-  )
+Widget BNBWidget {
+  return BottomNavigationBar(
+    items: [
+      BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+      BottomNavigationBarItem(icon: Icon(Icons.folder), label: "Formulario"),
+      BottomNavigationBarItem(icon: Icon(Icons.contact_mail), label: "Contato"),
+    ],
+    onTap: (value) => Navigator.pushNamed(context, routeName)),
+  );
 }
